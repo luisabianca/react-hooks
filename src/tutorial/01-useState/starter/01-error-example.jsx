82b36f076763b16e74d69ac51cf6d24fc47521e3
Bianca Santos <luisabianca93@gmail.com>
2023-11-10 20:41:17 -0300

    finish use state 01

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+
+  const [ count, setCount ] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+    console.log(count);
+  }
+
+  return (
+    <>
+      <h2>{count}</h2>
+      <button type='button' className='btn' onClick={handleClick}>
+        increment
+      </button>
+    </>
+  )
+
 };
 
 export default ErrorExample;
