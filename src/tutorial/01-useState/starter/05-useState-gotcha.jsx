37f32c41e6b41c84bcf728720dcbb5c8e20dd4f0
Bianca Santos <luisabianca93@gmail.com>
2023-11-10 22:51:25 -0300

    learn how to update current value console log use state

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+
+  const [value, setValue] = useState(0);
+
+  const handleClick = () => {
+    setValue((currentState) => { 
+      const newState = currentState + 1
+      console.log(newState);
+      return newState;
+    });
+  }
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={handleClick}>Increase</button>
+    </>
+  )
 };
 
 export default UseStateGotcha;
