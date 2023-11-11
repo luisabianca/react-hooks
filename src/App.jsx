22590479f10a0dc9e9694e805d9d87820ebecf64
Bianca Santos <luisabianca93@gmail.com>
2023-11-11 14:10:05 -0300

    learn render and re-render with use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Starter from './tutorial/02-useEffect/starter/01-code-example'
-import Final from './tutorial/02-useEffect/final/01-code-example'
+import Starter from './tutorial/02-useEffect/starter/02-useEffect-basics'
+import Final from './tutorial/02-useEffect/final/02-useEffect-basics'
 
 function App() {
   return (
     <div className='container'>
       <h2>Advanced React</h2>
-      <Starter />
+      <Starter /> 
       <Final />
     </div>
   ); 
