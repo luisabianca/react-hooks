da04489f66749a11e2e682e769b630bc1612afcf
Bianca Santos <luisabianca93@gmail.com>
2023-11-10 22:57:38 -0300

    learn settimeout on use state

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -5,11 +5,14 @@ const UseStateGotcha = () => {
   const [value, setValue] = useState(0);
 
   const handleClick = () => {
-    setValue((currentState) => { 
-      const newState = currentState + 1
-      console.log(newState);
-      return newState;
-    });
+    setTimeout(() => {
+      console.log('clicked the button')
+      setValue((currentState) => { 
+        const newState = currentState + 1
+        console.log(newState);
+        return newState;
+      });
+    }, 3000)
   }
 
   return (
