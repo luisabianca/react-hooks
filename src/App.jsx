ec228b5b8336a2143ce4c0eb58d06a027315000f
Bianca Santos <luisabianca93@gmail.com>
2023-11-12 20:09:11 -0300

    testing starter useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from './tutorial/02-useEffect/starter/02-useEffect-basics'
-import Final from './tutorial/02-useEffect/final/02-useEffect-basics'
+import Starter from './tutorial/02-useEffect/starter/03-multiple-effects'
+import Final from './tutorial/02-useEffect/final/03-multiple-effects'
 
 function App() {
   return (
