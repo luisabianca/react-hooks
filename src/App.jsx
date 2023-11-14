e2019a75609f344e0ac0fe952c8dbea5635defa2
Bianca Santos <luisabianca93@gmail.com>
2023-11-13 22:49:23 -0300

    finish fetch data using useEffect and useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Starter from './tutorial/02-useEffect/starter/03-multiple-effects'
-import Final from './tutorial/02-useEffect/final/03-multiple-effects'
+import Starter from './tutorial/02-useEffect/starter/04-fetch-data'
+import Final from './tutorial/02-useEffect/final/04-fetch-data'
 
 function App() {
   return (
     <div className='container'>
       <h2>Advanced React</h2>
-      {/* <Starter />  */}
-      <Final />
+      <Starter /> 
+      {/* <Final /> */}
     </div>
   ); 
 }
