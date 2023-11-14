536cefb4e25d632d43e5f1313c4ff391e1ebbed2
Bianca Santos <luisabianca93@gmail.com>
2023-11-13 23:39:57 -0300

    learn multiple return basic with useEffect

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,41 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const user = await response.json();
+        setUser(user);
+      } catch (error) {
+        console.log(error);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  },[])
+
+  const { avatar_url, name, company, bio } = user;
+  return (
+    <>
+      {isLoading ?
+        <h2>Loading...</h2>
+        :
+        <div>
+          <img 
+          style={{ width: '150px', borderRadius: '25px' }}
+          src={avatar_url} 
+          alt={name} />
+          <h2>{name}</h2>
+          <h4>Works At {company}</h4>
+          <p>{bio}</p>
+        </div>
+      }
+    </>
+  );
 };
 export default MultipleReturnsFetchData;
