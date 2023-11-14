4270d77b77b9c79039198f4e1d3b562eb7837424
Bianca Santos <luisabianca93@gmail.com>
2023-11-13 22:24:54 -0300

    multiple dependecies useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Starter from './tutorial/02-useEffect/starter/03-multiple-effects'
+import Final from './tutorial/02-useEffect/final/03-multiple-effects'
 
 function App() {
   return (
     <div className='container'>
       <h2>Advanced React</h2>
-      <Starter /> 
+      {/* <Starter />  */}
+      <Final />
     </div>
   ); 
 }
